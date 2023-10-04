9bb3f7a85f47c9c137d4ac83f715571c38151d87
JoaquinDE <joaquindonadell1@gmail.com>
2023-10-04 14:44:40 -0300

    commit joaco

--- buenSaborUml-main/buenSaborUml-main/src/main/java/com/tup/buensabor/entities/DetalleOrdenCompra.java
+++ buenSaborUml-main/buenSaborUml-main/src/main/java/com/tup/buensabor/entities/DetalleOrdenCompra.java
@@ -1,11 +1,14 @@
 package com.tup.buensabor.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Entity
 @Table(name = "DetalleOrdenCompra")
@@ -14,4 +17,19 @@ import lombok.Setter;
 @Getter
 @Setter
 public class DetalleOrdenCompra extends Base{
+
+    @NotNull
+    private Integer cantidadDetalleCompra;
+
+    @NotNull
+    private Double precioUnitarioDetalleCompra;
+
+    @ManyToOne()
+    @JoinColumn(name = "id_articulo_insumo")
+    private ArticuloInsumo articuloInsumo;
+
+    @ManyToOne()
+    @JoinColumn(name = "id_unidad_medida")
+    private UnidadMedida unidadMedida;
+
 }
