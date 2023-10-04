166d81188181854aa9e5d5aa5912eb63b39f25e2
JNeriv <136126908+JNeriv@users.noreply.github.com>
2023-10-04 00:34:10 -0300

    Nota de crédito agregada

---
+++ buenSaborUml-main/buenSaborUml-main/src/main/java/com/tup/buensabor/entities/NotaDeCredito.java
@@ -0,0 +1,37 @@
+package com.tup.buensabor.entities;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "NotaDeCredito")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class NotaDeCredito extends Base {
+
+    @NotNull
+    @Column(name = "Fecha_Nota_Credito")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaNotaDeCredito;
+
+    @Column(name = "Importe")
+    private double importeNDC;
+
+    @Column(name = "Descripción")
+    private String descripcionNDC;
+
+    @NotNull
+    @OneToOne
+    @JoinColumn(name = "id_factura")
+    private Factura factura;
+
+}
