d9f7e0f91e4afedfd2290b19c2217dc4cfadedc1
JNeriv <136126908+JNeriv@users.noreply.github.com>
2023-10-23 18:24:39 -0300

    Agregué service, controller y repository de UnidadMedida

--- ApiBack/demo/src/main/java/com/example/demo/repository/UnidadMedidaRepository.java
+++ ApiBack/demo/src/main/java/com/example/demo/repository/UnidadMedidaRepository.java
@@ -1,8 +1,44 @@
 package com.example.demo.repository;
 
-import com.example.demo.entities.UnidadMedida;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UnidadMedidaRepository extends JpaRepository<UnidadMedida, Long> {
-    // Puedes agregar métodos personalizados relacionados con UnidadMedida si es necesario
+import com.example.demo.entities.UnidadMedida;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UnidadMedidaRepository extends BaseRepository<UnidadMedida, Long> {
+
+    // Anotación método de Query
+    List<UnidadMedida> findByDenominacionContaining(String denominacion);
+
+    Page<UnidadMedida> findByDenominacionContaining(String denominacion, Pageable pageable);
+
+    boolean existsByAbreviatura(String abreviatura);
+
+
+    //Anotación JPQL parametros indexados
+    @Query(value = "SELECT u FROM UnidadMedida u WHERE u.denominacion LIKE '%?1%'")
+    List<UnidadMedida> search(String filtro);
+    @Query(value = "SELECT u FROM UnidadMedida u WHERE u.denominacion LIKE '%?1%'")
+    Page<UnidadMedida> search(String filtro, Pageable pageable);
+
+    //Anotación
+    @Query(
+            value = "SELECT * FROM unidadmedida WHERE unidadmedida.denominacion LIKE '%?1%'",
+            nativeQuery = true
+    )
+    List<UnidadMedida> searchNativo(String filtro);
+    @Query(
+            value = "SELECT * FROM unidadmedida WHERE unidadmedida.denominacion LIKE '%?1%'",
+            countQuery = "SELECT count(*) FROM unidadmedida",
+            nativeQuery = true
+    )
+    Page<UnidadMedida> searchNativo(String filtro, Pageable pageable);
+
+
 }
