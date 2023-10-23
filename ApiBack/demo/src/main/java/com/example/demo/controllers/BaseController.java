a9eab5a5d5400616f241e684991c177dd7859bc1
Franco <fgimenez185@gmail.com>
2023-10-22 21:05:07 -0300

    agregue el repository de comprobante

---
+++ ApiBack/demo/src/main/java/com/example/demo/controllers/BaseController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controllers;
+
+import com.example.demo.entities.Base;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.Serializable;
+
+public interface BaseController<E extends Base, ID extends Serializable> {
+
+    @GetMapping("")
+    public ResponseEntity<?> getAll();
+    public ResponseEntity<?> getAll(Pageable pageable);
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOne(@PathVariable ID id);
+    @PostMapping("")
+    public ResponseEntity<?> save(@RequestBody E entity);
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable ID id,@RequestBody E entity);
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable ID id);
+}
