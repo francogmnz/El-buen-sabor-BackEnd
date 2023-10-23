ff72cf35bd51f3e2375fb8fc8ee012b4ec6ad75a
MartinAlejandroMelonari <144966823+MartinAlejandroMelonari@users.noreply.github.com>
2023-10-23 18:39:07 -0300

    clases Producto, Usuario y DetalleOrdenCompra
    clases creadas, services y controllers

---
+++ ApiBack/demo/src/main/java/com/example/demo/services/DetalleOrdenCompraService.java
@@ -0,0 +1,6 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.DetalleOrdenCompra;
+
+public interface DetalleOrdenCompraService extends BaseService<DetalleOrdenCompra, Long>{
+}
---
+++ ApiBack/demo/src/main/java/com/example/demo/services/UsuarioService.java
@@ -0,0 +1,6 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.Usuario;
+
+public interface UsuarioService extends BaseService<Usuario, Long>{
+}
