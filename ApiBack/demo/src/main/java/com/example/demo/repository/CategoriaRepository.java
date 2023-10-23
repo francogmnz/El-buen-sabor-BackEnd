3283063b5b241217ea630be84939112d0d8a253f
Franco <fgimenez185@gmail.com>
2023-10-23 18:56:26 -0300

    cambio de nombre de repositroy categoria y agregacion de ingredienteProducto repository

--- ApiBack/demo/src/main/java/com/example/demo/repository/CategoriaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entities.RubroProducto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<RubroProducto, Long> {
-    // Puedes agregar métodos personalizados relacionados con Categoria si es necesario
-}
