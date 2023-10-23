b74b39084e76fbfa2fc1d9107102576f2a62b8f8
Franco <fgimenez185@gmail.com>
2023-10-23 17:57:39 -0300

    .

--- ApiBack/demo/src/main/java/com/example/demo/controllers/RubroIngredienteController.java
+++ ApiBack/demo/src/main/java/com/example/demo/controllers/RubroIngredienteController.java
@@ -1,4 +1,13 @@
 package com.example.demo.controllers;
 
-public class RubroIngredienteController {
+import com.example.demo.entities.Factura;
+import com.example.demo.services.FacturaServiceImpl;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+ @RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(path = "api/v1/rubroIngrediente")
+public class RubroIngredienteController extends BaseControllerImpl<Factura, FacturaServiceImpl>{
 }
