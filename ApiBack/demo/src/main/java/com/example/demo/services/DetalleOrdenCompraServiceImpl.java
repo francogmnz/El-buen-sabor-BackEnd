a80f6f35ca3f635adb034248a5bed966995eb231
MartinAlejandroMelonari <144966823+MartinAlejandroMelonari@users.noreply.github.com>
2023-10-24 14:39:49 -0300

    Corregi unos ServiceImpl
    Agregué funciones en los Service Impl de Producto, Usuario y DetalleOrdenCompra

--- ApiBack/demo/src/main/java/com/example/demo/services/DetalleOrdenCompraServiceImpl.java
+++ ApiBack/demo/src/main/java/com/example/demo/services/DetalleOrdenCompraServiceImpl.java
@@ -1,10 +1,15 @@
 package com.example.demo.services;
 
 import com.example.demo.entities.DetalleOrdenCompra;
+import com.example.demo.entities.Producto;
 import com.example.demo.repository.BaseRepository;
 import com.example.demo.repository.DetalleOrdenCompraRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DetalleOrdenCompraServiceImpl extends BaseServiceImpl<DetalleOrdenCompra, Long> implements DetalleOrdenCompraService{
@@ -13,5 +18,35 @@ public class DetalleOrdenCompraServiceImpl extends BaseServiceImpl<DetalleOrdenC
 
     public DetalleOrdenCompraServiceImpl(BaseRepository<DetalleOrdenCompra, Long> baseRepository, DetalleOrdenCompraRepository detalleOrdenCompraRepository) {
         super(baseRepository);
+    }
+
+    @Override
+    public List<DetalleOrdenCompra> findAll() throws Exception {
+        return null;
+    }
+
+    @Override
+    public Page<DetalleOrdenCompra> findAll(Pageable pageable) throws Exception {
+        return null;
+    }
+
+    @Override
+    public DetalleOrdenCompra findById(Long aLong) throws Exception {
+        return null;
+    }
+
+    @Override
+    public DetalleOrdenCompra save(DetalleOrdenCompra entity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public DetalleOrdenCompra update(Long aLong, DetalleOrdenCompra entity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long aLong) throws Exception {
+        return false;
     }
 }
--- ApiBack/demo/src/main/java/com/example/demo/services/ProductoServiceImpl.java
+++ ApiBack/demo/src/main/java/com/example/demo/services/ProductoServiceImpl.java
@@ -1,9 +1,14 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.Localidad;
 import com.example.demo.entities.Producto;
 import com.example.demo.repository.BaseRepository;
 import com.example.demo.repository.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public class ProductoServiceImpl extends BaseServiceImpl<Producto, Long> implements ProductoService{
     @Autowired
@@ -11,5 +16,35 @@ public class ProductoServiceImpl extends BaseServiceImpl<Producto, Long> impleme
 
     public ProductoServiceImpl(BaseRepository<Producto, Long> baseRepository, ProductoRepository productoRepository) {
         super(baseRepository);
+    }
+
+    @Override
+    public List<Producto> findAll() throws Exception {
+        return null;
+    }
+
+    @Override
+    public Page<Producto> findAll(Pageable pageable) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Producto findById(Long aLong) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Producto save(Producto entity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Producto update(Long aLong, Producto entity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long aLong) throws Exception {
+        return false;
     }
 }
