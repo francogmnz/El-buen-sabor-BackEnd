572bdd69497f5bf5782e27d4e8c5b524c611f773
Franco <fgimenez185@gmail.com>
2023-10-24 12:25:45 -0300

    arreglo error clase cliente impl

--- ApiBack/demo/src/main/java/com/example/demo/services/ClienteServiceImpl.java
+++ ApiBack/demo/src/main/java/com/example/demo/services/ClienteServiceImpl.java
@@ -6,8 +6,10 @@ import com.example.demo.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+@Service
 
 public class ClienteServiceImpl extends BaseServiceImpl<Cliente,Long>  implements ClienteService {
 
