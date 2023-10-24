87be1e1af04be1392ac09868528a9836f0e338ed
Franco <fgimenez185@gmail.com>
2023-10-24 12:07:30 -0300

    arreglo clase localidad

--- ApiBack/demo/src/main/java/com/example/demo/entities/Localidad.java
+++ ApiBack/demo/src/main/java/com/example/demo/entities/Localidad.java
@@ -1,5 +1,18 @@
 package com.example.demo.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "localidad")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Localidad  extends Base{
     private int id_localidad;
     private String descripcionLocalidad;
