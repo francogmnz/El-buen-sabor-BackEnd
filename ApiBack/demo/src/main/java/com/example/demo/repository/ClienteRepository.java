8d08dd940665e1c0965af66bbfb4e97c94dd5f49
Federico Ibañez <federicosjrc55@gmail.com>
2023-10-25 19:09:12 -0300

    Update ClienteRepository.java

--- ApiBack/demo/src/main/java/com/example/demo/repository/ClienteRepository.java
+++ ApiBack/demo/src/main/java/com/example/demo/repository/ClienteRepository.java
@@ -1,11 +1,40 @@
 package com.example.demo.repository;
 
 import com.example.demo.entities.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
-@Repository
+import java.util.List;
 
-public interface ClienteRepository extends BaseRepository<Cliente, Long> {
-    // Puedes agregar métodos personalizados relacionados con Cliente si es necesario
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    // Anotación método de Query
+    List<Cliente> findByApellidoContaining(String apellido);
+
+    Page<Cliente> findByApellidoContaining(String apellido, Pageable pageable);
+
+    boolean existsByApellido(String apellido);
+
+
+    //Anotación JPQL parametros indexados
+    @Query(value = "SELECT c FROM Cliente c WHERE c.apellido LIKE '%?1%'")
+    List<Cliente> search(String filtro);
+    @Query(value = "SELECT c FROM Cliente c WHERE c.apellido LIKE '%?1%'")
+    Page<Cliente> search(String filtro, Pageable pageable);
+
+    //Anotación
+    @Query(
+            value = "SELECT * FROM Cliente WHERE cliente.apellido LIKE '%?1%'",
+            nativeQuery = true
+    )
+    List<Cliente> searchNativo(String filtro);
+    @Query(
+            value = "SELECT * FROM cliente WHERE cliente.apellido LIKE '%?1%'",
+            countQuery = "SELECT count(*) FROM cliente",
+            nativeQuery = true
+    )
+    Page<Cliente> searchNativo(String filtro, Pageable pageable);
+
+
 }
