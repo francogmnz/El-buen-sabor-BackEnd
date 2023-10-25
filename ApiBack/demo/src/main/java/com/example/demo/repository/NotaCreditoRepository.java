ecc111edc8f43fbf6219a5ea828e63aa3c40bd45
Federico Ibañez <federicosjrc55@gmail.com>
2023-10-25 20:02:30 -0300

    controllers y repositoris

--- ApiBack/demo/src/main/java/com/example/demo/repository/NotaCreditoRepository.java
+++ ApiBack/demo/src/main/java/com/example/demo/repository/NotaCreditoRepository.java
@@ -1,11 +1,42 @@
 package com.example.demo.repository;
 
 import com.example.demo.entities.NotaCredito;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 
 public interface NotaCreditoRepository extends BaseRepository<NotaCredito, Long> {
-    // Puedes agregar métodos personalizados relacionados con NotaDeCredito si es necesario
+    // Anotación método de Query
+    List<NotaCredito> findByDescripcionNCContaining(String descripcionNC);
+
+    Page<NotaCredito> findByDescripcionNCContaining(String descripcionNC, Pageable pageable);
+
+    boolean existsBydescripcionNC(String descripcionNC);
+
+
+    //Anotación JPQL parametros indexados
+    @Query(value = "SELECT c FROM NotaCredito c WHERE c.descripcionNC LIKE '%?1%'")
+    List<NotaCredito> search(String filtro);
+    @Query(value = "SELECT c FROM NotaCredito c WHERE c.descripcionNC LIKE '%?1%'")
+    Page<NotaCredito> search(String filtro, Pageable pageable);
+
+    //Anotación
+    @Query(
+            value = "SELECT * FROM NotaCredito WHERE NotaCredito.descripcionNC LIKE '%?1%'",
+            nativeQuery = true
+    )
+    List<NotaCredito> searchNativo(String filtro);
+    @Query(
+            value = "SELECT * FROM NotaCredito WHERE NotaCredito.descripcionNC LIKE '%?1%'",
+            countQuery = "SELECT count(*) FROM NotaCredito",
+            nativeQuery = true
+    )
+    Page<NotaCredito> searchNativo(String filtro, Pageable pageable);
+
+
 }
--- ApiBack/demo/src/main/java/com/example/demo/services/ClienteServiceImpl.java
+++ ApiBack/demo/src/main/java/com/example/demo/services/ClienteServiceImpl.java
@@ -1,8 +1,10 @@
 package com.example.demo.services;
 
 import com.example.demo.entities.Cliente;
+import com.example.demo.entities.UnidadMedida;
 import com.example.demo.repository.BaseRepository;
 import com.example.demo.repository.ClienteRepository;
+import com.example.demo.repository.UnidadMedidaRepository;
 import com.example.demo.services.BaseServiceImpl;
 import com.example.demo.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +16,38 @@ import java.util.List;
 
 @Service
 public class ClienteServiceImpl extends BaseServiceImpl<Cliente,Long>{
-    public ClienteServiceImpl(BaseRepository<Cliente, Long> baseRepository) {
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public ClienteServiceImpl(BaseRepository<Cliente, Long> baseRepository, ClienteRepository clienteRepository) {
         super(baseRepository);
+        this.clienteRepository = clienteRepository;
+    }
+
+    @Override
+    public List<Cliente> search(String filtro) throws Exception {
+        try {
+            //List<UnidadMedida> unidadesmedida = unidadmedidaRepository.findByDenominacionContaining(filtro, filtro);
+            //List<UnidadMedida> unidadesmedida = unidadmedidaRepository.search(filtro);
+            List<Cliente> clientes = clienteRepository.searchNativo(filtro);
+
+            return clientes;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    public Page<Cliente> search(String filtro, Pageable pageable) throws Exception {
+        try {
+            //Page<UnidadMedida> unidadesmedida = unidadmedidaRepository.findByDenominacionContaining(filtro, filtro, pageable);
+            //Page<UnidadMedida> unidadesmedida = unidadmedidaRepository.search(filtro, pageable);
+            Page<Cliente> cliente = clienteRepository.searchNativo(filtro, pageable);
+
+            return cliente;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 }
 
