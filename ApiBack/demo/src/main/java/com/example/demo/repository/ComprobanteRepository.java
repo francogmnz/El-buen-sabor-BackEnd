9c24d8f6f672f6f06076e2339e8a3ca71a1e0282
Federico Ibañez <federicosjrc55@gmail.com>
2023-10-25 19:11:15 -0300

    Update ComprobanteRepository.java

--- ApiBack/demo/src/main/java/com/example/demo/repository/ComprobanteRepository.java
+++ ApiBack/demo/src/main/java/com/example/demo/repository/ComprobanteRepository.java
@@ -1,9 +1,42 @@
 package com.example.demo.repository;
 
 import com.example.demo.entities.Comprobante;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
-@Repository
-public interface ComprobanteRepository extends BaseRepository <Comprobante, Long> {
+import java.util.List;
+
+public interface ComprobanteRepository extends JpaRepository <Comprobante, Long> {
+
+    // Anotación método de Query
+    List<Comprobante> findByNumeroContaining(String numero);
+
+    Page<Comprobante> findByNumeroContaining(String numero, Pageable pageable);
+
+    boolean existsByNumero(String numero);
+
+
+    //Anotación JPQL parametros indexados
+    @Query(value = "SELECT c FROM Comprobante c WHERE c.numero LIKE '%?1%'")
+    List<Comprobante> search(String filtro);
+    @Query(value = "SELECT c FROM Comprobante c WHERE c.numero LIKE '%?1%'")
+    Page<Comprobante> search(String filtro, Pageable pageable);
+
+    //Anotación
+    @Query(
+            value = "SELECT * FROM comprobante WHERE comprobante.numero LIKE '%?1%'",
+            nativeQuery = true
+    )
+    List<Comprobante> searchNativo(String filtro);
+    @Query(
+            value = "SELECT * FROM comprobante WHERE comprobante.numero  LIKE '%?1%'",
+            countQuery = "SELECT count(*) FROM comprobante",
+            nativeQuery = true
+    )
+    Page<Comprobante> searchNativo(String filtro, Pageable pageable);
+
+
+
 }
