69b62d957d9dbe0954b8efd7f840b5187e61f179
MartinAlejandroMelonari <144966823+MartinAlejandroMelonari@users.noreply.github.com>
2023-10-25 14:39:23 -0300

    Querys agregados en UsuarioRepository
    UsuarioRepository ahora tiene los Querys

--- ApiBack/demo/src/main/java/com/example/demo/repository/UsuarioRepository.java
+++ ApiBack/demo/src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -1,11 +1,25 @@
 package com.example.demo.repository;
 
+import com.example.demo.entities.UnidadMedida;
 import com.example.demo.entities.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 
 public interface UsuarioRepository extends BaseRepository<Usuario, Long> {
-    // Puedes agregar métodos personalizados relacionados con Usuario si es necesario
+    List<Usuario> findByUsernameContaining(String username);
+    Page<Usuario> findByUsernameContaining(String username, Pageable pageable);
+
+    boolean existsByAuth0Id(String auth0Id);
+
+    @Query(value = "SELECT u FROM Usuario u WHERE u.username LIKE '%?1%'")
+    List<Usuario> search(String filtro);
+    @Query(value = "SELECT u FROM Usuario u WHERE u.username LIKE '%?1%'")
+    Page<Usuario> search(String filtro, Pageable pageable);
 }
